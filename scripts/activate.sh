3850afd15ea3c4c0fb524e7cc7a5970620cede7c
RayanWang7 <rayanwang@linkernetworks.com>
2021-11-16 11:22:14 +0800

    close the venv.

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -39,40 +39,40 @@ fi
 export PYAV_PYTHON
 export PYAV_PIP="${PYAV_PIP-$PYAV_PYTHON -m pip}"
 
-if [[ "$GITHUB_ACTION" || "$TRAVIS" ]]; then
+# if [[ "$GITHUB_ACTION" || "$TRAVIS" ]]; then
 
-    # GitHub/Travis as a very self-contained environment. Lets just work in that.
-    echo "We're on CI, so not setting up another virtualenv."
+#     # GitHub/Travis as a very self-contained environment. Lets just work in that.
+#     echo "We're on CI, so not setting up another virtualenv."
 
-    if [[ "$TRAVIS_PYTHON_VERSION" = "2.7" || "$TRAVIS_PYTHON_VERSION" = "pypy" ]]; then
-        PYAV_PYTHON=python
-        PYAV_PIP=pip
-    fi
+#     if [[ "$TRAVIS_PYTHON_VERSION" = "2.7" || "$TRAVIS_PYTHON_VERSION" = "pypy" ]]; then
+#         PYAV_PYTHON=python
+#         PYAV_PIP=pip
+#     fi
 
-else
+# else
 
-    export PYAV_VENV_NAME="$(uname -s).$(uname -r).$("$PYAV_PYTHON" -c '
-from __future__ import print_function
-import sys
-import platform
-print("{}{}.{}".format(platform.python_implementation().lower(), *sys.version_info[:2]))
-    ')"
-    export PYAV_VENV="$PYAV_ROOT/venvs/$PYAV_VENV_NAME"
+#     export PYAV_VENV_NAME="$(uname -s).$(uname -r).$("$PYAV_PYTHON" -c '
+# from __future__ import print_function
+# import sys
+# import platform
+# print("{}{}.{}".format(platform.python_implementation().lower(), *sys.version_info[:2]))
+#     ')"
+#     export PYAV_VENV="$PYAV_ROOT/venvs/$PYAV_VENV_NAME"
 
-    if [[ ! -e "$PYAV_VENV/bin/python" ]]; then
-        mkdir -p "$PYAV_VENV"
-        virtualenv -p "$PYAV_PYTHON" "$PYAV_VENV"
-        "$PYAV_VENV/bin/pip" install --upgrade pip setuptools
-    fi
+#     if [[ ! -e "$PYAV_VENV/bin/python" ]]; then
+#         mkdir -p "$PYAV_VENV"
+#         virtualenv -p "$PYAV_PYTHON" "$PYAV_VENV"
+#         "$PYAV_VENV/bin/pip" install --upgrade pip setuptools
+#     fi
 
-    if [[ -e "$PYAV_VENV/bin/activate" ]]; then
-        source "$PYAV_VENV/bin/activate"
-    else
-        # Not a virtualenv (perhaps a debug Python); lets manually "activate" it.
-        PATH="$PYAV_VENV/bin:$PATH"
-    fi
+#     if [[ -e "$PYAV_VENV/bin/activate" ]]; then
+#         source "$PYAV_VENV/bin/activate"
+#     else
+#         # Not a virtualenv (perhaps a debug Python); lets manually "activate" it.
+#         PATH="$PYAV_VENV/bin:$PATH"
+#     fi
 
-fi
+# fi
 
 
 # Just a flag so that we know this was supposedly run.
